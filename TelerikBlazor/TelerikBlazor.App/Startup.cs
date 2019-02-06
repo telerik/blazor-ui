d93211cc54250e5cfc879e7d9234bb6f32e14968
Bozhidar Ivanchev <Bozhidar.Ivanchev@telerik.com>
2019-02-06 17:56:23 +0200

    chore: add use secrets

--- TelerikBlazor/TelerikBlazor.App/Startup.cs
+++ TelerikBlazor/TelerikBlazor.App/Startup.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Components.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using TelerikBlazor.App.Models;
 using TelerikBlazor.App.Services;
@@ -7,12 +9,19 @@ namespace TelerikBlazor.App
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+        public Startup() => Configuration = new ConfigurationBuilder().AddUserSecrets<Startup>().Build();
+
         public void ConfigureServices(IServiceCollection services)
         {
             // Example of a data service
             services.AddSingleton<WeatherForecastService>();
 
-            services.AddSingleton(new NorthwindContext());
+            var connection = Configuration["ConnectionStrings:NorthwindDB"];
+            var options = new DbContextOptionsBuilder<NorthwindContext>()
+                                .UseSqlServer(connection)
+                                .Options;
+            services.AddSingleton(new NorthwindContext(options));
 
             services.AddKendoBlazor();
         }
