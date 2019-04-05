3f19670a2f0d29917935bf3a93171901216c5838
Angel Petrov <agpetrov@progress.com>
2019-04-05 10:47:53 +0300

    chore: add code view for demos

---
+++ TelerikBlazor.Client/wwwroot/js/codeManager.js
@@ -0,0 +1,18 @@
+﻿window.codeManager = {
+    renderCode: function (url) {
+
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', '/source/index?path=/Pages/' + url+'.cshtml');
+        xhr.onload = function () {
+            if (xhr.status === 200) {
+                var snippetContainer = document.getElementById("codecontainer");
+                snippetContainer.innerHTML = xhr.responseText;
+                prettyPrint();
+            }
+            else {
+                alert('Request failed.  Returned status of ' + xhr.status);
+            }
+        };
+        xhr.send();
+    }
+}
