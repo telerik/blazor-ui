2a03c0d8f7c17b58d75298954c7a15083f6abfba
Marin Bratanov <m.bratanov@gmail.com>
2019-10-18 17:59:49 +0300

    grid custom popup edit form

---
+++ grid/custom-popup-form/Data/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace custom_popup_form.Data
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Enter a name")]
+        [StringLength(10, ErrorMessage = "That name is too long")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Enter a last name")]
+        [StringLength(10, ErrorMessage = "That name is too long")]
+        public string LastName { get; set; }
+        [Required]
+        [Range(typeof(DateTime), "1/1/1999", "1/12/2019",
+            ErrorMessage = "Value for {0} must be between {1:dd MMM yyyy} and {2:dd MMM yyyy}")]
+        [Display(Name = "Hire Date")]
+        public DateTime HireDate { get; set; }
+        public bool OnLeave { get; set; }
+    }
+}
---
+++ grid/custom-popup-form/readme.md
@@ -0,0 +1,3 @@
+This example shows how you can implement a custom popup editor with an entirely custom template and layout.
+
+The built-in [popup editing](https://docs.telerik.com/blazor-ui/components/grid/editing/popup) supports [editor templates](https://docs.telerik.com/blazor-ui/components/grid/templates#edit-template) for the given field, but it does not provide full flexibility on the layout.
