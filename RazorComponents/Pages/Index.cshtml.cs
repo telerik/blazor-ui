0899c0f8c9657585d4e0e3710dbf8d994375a9ec
Shannon Pence <spence@craftcruises.com>
2019-11-08 00:31:07 -0600

    Razor Component Example
    Includes working MS razor Component and a Telerik Window.

---
+++ RazorComponents/Pages/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using BlazorInRazorPages.Models;
+
+namespace BlazorInRazorPages.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public IEnumerable<Customer> BlazorGridData { get; set; }
+
+        public void OnGet()
+        {
+            BlazorGridData = Enumerable.Range(1, 200).Select(x => new Customer { Id = x, Name = "Name " + x });
+        }
+    }
+}
