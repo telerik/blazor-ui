849180144114e92f0a44cd87c12382fbb5eb6f84
Marin Bratanov <m.bratanov@gmail.com>
2019-11-17 17:58:04 +0200

    first version of server localization sample

---
+++ common/localization/ServerLocalizationResx/Controllers/CultureController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ServerLocalizationResx.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class CultureController : Controller
+    {
+        public IActionResult SetCulture(string culture, string redirectUri)
+        {
+            if (culture != null)
+            {
+                HttpContext.Response.Cookies.Append(
+                    CookieRequestCultureProvider.DefaultCookieName,
+                    CookieRequestCultureProvider.MakeCookieValue(new RequestCulture("en-US", culture)));
+            }
+
+            return LocalRedirect(redirectUri);
+        }
+
+        public IActionResult ResetCulture(string redirectUri)
+        {
+            HttpContext.Response.Cookies.Delete(CookieRequestCultureProvider.DefaultCookieName);
+
+            return LocalRedirect(redirectUri);
+        }
+    }
+}
