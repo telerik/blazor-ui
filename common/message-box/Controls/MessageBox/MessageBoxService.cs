2c9ce4249ff594c60059cff2f3cba59ce53b3ba3
Sylvain BLANCHARD <sylvain.blanchard@dlmsoft.fr>
2019-12-23 15:21:55 +0100

    Add MessageBoxService class

---
+++ common/message-box/Controls/MessageBox/MessageBoxService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MessageBoxControl.Controls.MessageBox
+{
+    public class MessageBoxService
+    {
+        public static event Action<string, string, MessageBoxButtonTypeEnum, Action<MessageBoxResultEnum>> OnShowMessageBox;
+
+        public void Show(string title, string message, MessageBoxButtonTypeEnum buttonType, Action<MessageBoxResultEnum> actionToInvoke)
+        {
+            OnShowMessageBox.Invoke(title, message, buttonType, actionToInvoke);
+        }
+    }
+
+    public enum MessageBoxButtonTypeEnum
+    {
+        OK = 1,
+        OKCancel = 2,
+        YesNo = 3,
+        YesNoCancel = 4
+    }
+
+    public enum MessageBoxResultEnum
+    {
+        OK = 1,
+        Cancel = 2,
+        Yes = 3,
+        No = 4
+    }
+}
