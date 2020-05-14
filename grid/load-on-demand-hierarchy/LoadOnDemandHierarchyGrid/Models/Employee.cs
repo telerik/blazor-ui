6644eb757d51bac5092784943b92ea9dace2fcab
Svetoslav Dimitrov <svdimitr@progress.com>
2020-05-14 18:12:40 +0300

    feat(grid): example of hierarchy grid with load on demand

---
+++ grid/load-on-demand-hierarchy/LoadOnDemandHierarchyGrid/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoadOnDemandHierarchyGrid.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
