019631636156239d7dc6af5a6ab63fe6a920c573
Svetoslav Dimitrov <svdimitr@progress.com>
2020-06-02 12:46:45 +0300

    feat(scheduler): ics-converter project

---
+++ scheduler/ics-data-convertion/ICS_Data_Convertion/ICS_Data_Convertion/Models/CalendarAppointments.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using ICS_Data_Convertion.Services;
+
+namespace ICS_Data_Convertion.Models
+{
+    public class CalendarAppointments
+    {
+        ConvertStringToDateTime Converter = new ConvertStringToDateTime();
+        private string _dtStart { get; set; }
+        private string _dtEnd { get; set; }
+        public string DTStart 
+        {
+            get
+            {
+                return _dtStart;
+            }
+            set
+            {
+                _dtStart = value;
+                Start = Converter.ConvertStringToDate(value);
+            }
+        }
+        public string DTEnd
+        {
+            get
+            {
+                return _dtEnd;
+            }
+            set
+            {
+                _dtEnd = value;
+                End = Converter.ConvertStringToDate(value);
+            }
+        }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime? Start { get; set; }
+        public DateTime? End { get; set; }
+    }
+}
---
+++ scheduler/ics-data-convertion/ICS_Data_Convertion/ICS_Data_Convertion/Services/ConvertStringToDateTime.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ICS_Data_Convertion.Services
+{
+    public class ConvertStringToDateTime
+    {
+        public DateTime? ConvertStringToDate(string dateTime)
+        {
+            if (!String.IsNullOrEmpty(dateTime))
+            {
+                var date = new DateTime();
+
+                //This is a simple datetime parser. According to the iCal standard, a DateTime string ending in 'Z' indicates that the datetime is in UTC rather than local time. This code assumes the date is in UTC.
+                if (DateTime.TryParseExact(dateTime.ToLower().Replace("t", " ").Replace("z", ""), "yyyyMMdd HHmmss", CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal, out date))
+                {
+                    return date;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
