3b4af6b45c347ae58df71a79599066c2b32ccdfe
Marin Bratanov <m.bratanov@gmail.com>
2020-08-02 10:30:29 +0300

    feat(editor): allow export to multiple formats

--- editor/ImportExport/EditorImportExport/Data/FileConverter.cs
+++ editor/ImportExport/EditorImportExport/Data/FileConverter.cs
@@ -22,17 +22,28 @@ namespace EditorImportExport.Data
         private IWebHostEnvironment Environment { get; set; }
         private IJSRuntime _js { get; set; }
 
+        /// <summary>
+        /// Import a file from disk and convert it to an HTML string for use in the Editor
+        /// </summary>
+        /// <returns>An HTML string that is just the contents of the body tag so the editor can work with them</returns>
         public string GetHtmlString()
         {
             try
             {
-                RadFlowDocument document = ReadFile("JohnGrisham.docx");
+                // read the file - in this sample a hardcoded path
+                string path = Path.Combine(Environment.WebRootPath, "JohnGrisham.docx");
+                RadFlowDocument document = ReadFile(path);
+
+                // convert the file to HTML
                 HtmlFormatProvider provider = new HtmlFormatProvider();
                 provider.ExportSettings.DocumentExportLevel = DocumentExportLevel.Fragment;
                 string html = provider.Export(document);
+
+                // get only the <body> contents
                 int bodyStart = html.IndexOf("<body>") + "<body>".Length;
                 int bodyEnd = html.IndexOf("</body>");
                 string body = html.Substring(bodyStart, bodyEnd - bodyStart);
+
                 return body;
             }
             catch(Exception ex)
@@ -41,29 +52,63 @@ namespace EditorImportExport.Data
             }
         }
 
-        public async Task ExportHtmlFileToPdf(string htmlContent)
+        /// <summary>
+        /// Convert the HTML string to a file, download the generated file in the browser
+        /// </summary>
+        /// <param name="htmlContent">The HTML content to export</param>
+        /// <param name="fileName">The FileName of the downloaded file, its extension is used to fetch the exprot provider</param>
+        public async Task ExportAndDownloadHtmlContent(string htmlContent, string fileName)
         {
             try
             {
+                // prepare a document with the HTML content that we can use for conversion
                 HtmlFormatProvider provider = new HtmlFormatProvider();
                 RadFlowDocument document = provider.Import(htmlContent);
 
-                pdfProviderNamespace.PdfFormatProvider pdfProvider = new pdfProviderNamespace.PdfFormatProvider();
-                byte[] pdfFileBytes = pdfProvider.Export(document);
+                // get the provider to export and then download the file
+                string mimeType;
+                IFormatProvider<RadFlowDocument> exportProvider = GetExportFormatProvider(fileName, out mimeType);
+                byte[] exportFileBytes = null;
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    exportProvider.Export(document, ms);
+                    exportFileBytes = ms.ToArray();
+                }
 
-                await FileDownloader.Save(_js, pdfFileBytes, "application/pdf", "EditorContent.pdf");
+                // download the file in the browser
+                await FileDownloader.Save(_js, exportFileBytes, mimeType, fileName);
             }
             catch { }
         }
 
-        RadFlowDocument ReadFile(string fileName)
+        /// <summary>
+        /// Reads the file from the disk as an in-memory document, based on the provided path
+        /// </summary>
+        /// <param name="path">Path to the file on disk you wish to import</param>
+        /// <returns>RadFlowDocument that you can use to convert the contents to other formats</returns>
+        RadFlowDocument ReadFile(string path)
         {
-            IFormatProvider<RadFlowDocument> fileFormatProvider = null;
-            RadFlowDocument document = null;
+            IFormatProvider<RadFlowDocument> fileFormatProvider = GetImportFormatProvider(Path.GetExtension(path));
 
-            string path = Path.Combine(Environment.WebRootPath, fileName);
+            RadFlowDocument document;
 
-            switch (Path.GetExtension(path))
+            using (FileStream input = new FileStream(path, FileMode.Open, FileAccess.Read))
+            {
+                document = fileFormatProvider.Import(input);
+            }
+
+            return document;
+        }
+
+        /// <summary>
+        /// Get Supported providers for the import operation based on the file extension
+        /// </summary>
+        /// <param name="extension">the extension of the file we will import, used to discern the provider type</param>
+        /// <returns>IFormatProvider<RadFlowDocument> that you can use to read that file and import it as an in-memory document you can convert to other formats</returns>
+        IFormatProvider<RadFlowDocument> GetImportFormatProvider(string extension)
+        {
+            IFormatProvider<RadFlowDocument> fileFormatProvider;
+            switch (extension)
             {
                 case ".docx":
                     fileFormatProvider = new DocxFormatProvider();
@@ -82,15 +127,63 @@ namespace EditorImportExport.Data
                     break;
             }
 
-            using (FileStream input = new FileStream(path, FileMode.Open, FileAccess.Read))
+            if (fileFormatProvider == null)
             {
-                document = fileFormatProvider.Import(input);
+                throw new NotSupportedException("The chosen file cannot be read with the listed providers.");
             }
 
-            return document;
+            return fileFormatProvider;
         }
 
+        /// <summary>
+        /// Get Supported providers for the export operation based on the file name, also gets its MIME type as an out parameter
+        /// </summary>
+        /// <param name="fileName">the file name you wish to export, the provider is discerned based on its extensiom</param>
+        /// <param name="mimeType">an out parameter with the MIME type for this file so you can download it</param>
+        /// <returns>IFormatProvider<RadFlowDocument> that you can use to export the original document to a certain file format</returns>
+        IFormatProvider<RadFlowDocument> GetExportFormatProvider(string fileName, out string mimeType)
+        {
+            // we get both the provider and the MIME type to use only one swtich-case
+            IFormatProvider<RadFlowDocument> fileFormatProvider;
+            string extension = Path.GetExtension(fileName);
+            switch (extension)
+            {
+                case ".docx":
+                    fileFormatProvider = new DocxFormatProvider();
+                    mimeType = "application/vnd.openxmlformats-officedocument.wordprocessingml.document";
+                    break;
+                case ".rtf":
+                    fileFormatProvider = new RtfFormatProvider();
+                    mimeType = "application/rtf";
+                    break;
+                case ".html":
+                    fileFormatProvider = new HtmlFormatProvider();
+                    mimeType = "text/html";
+                    break;
+                case ".txt":
+                    fileFormatProvider = new TxtFormatProvider();
+                    mimeType = "text/plain";
+                    break;
+                case ".pdf":
+                    fileFormatProvider = new pdfProviderNamespace.PdfFormatProvider();
+                    mimeType = "application/pdf";
+                    break;
+                default:
+                    fileFormatProvider = null;
+                    mimeType = string.Empty;
+                    break;
+            }
 
+            if (fileFormatProvider == null)
+            {
+                throw new NotSupportedException("The chosen format cannot be exported with the listed providers.");
+            }
+
+            return fileFormatProvider;
+        }
+
+        // DI for the environment feature we need - path to the wwwroot folder to read the intial content,
+        // and JS runtime for downloading the file to the browser
         public FileConverter(IWebHostEnvironment env, IJSRuntime js)
         {
             Environment = env;
--- editor/ImportExport/readme.md
+++ editor/ImportExport/readme.md
@@ -4,31 +4,31 @@ The [Blazor Editor component](https://docs.telerik.com/blazor-ui/components/edit
 
 This sample project shows how to:
 
-* Convert a .docx file to an HTML string and put it in the editor
-* Convert the HTML from the editor to a PDF and download it
+* [Import](#import) - Convert a .docx file to an HTML string and put it in the editor (can also work with some other formats).
+* [Export](#export) - Convert the HTML from the editor to a PDF and other formats and download it.
 
 You can also extend it to use other formats.
 
-The sample also increases the SignalR maximum message size in `Startup.cs` to allow for file downloads, and to let large content travel to the browser (such as the editor content, e.g., when you click the View HTML button).
+The sample also increases the SignalR maximum message size in `Startup.cs` to allow for file downloads, and to let large content travel to the browser (such as the editor content, e.g., when you click the View HTML button, or the files themselves).
 
-## DOCX Import
+## Import
 
 The conversion happens in the `FileConverter.cs` file - the `GetHtmlString` method.
 
 In this sample, it reads a `docx` file in the `wwwroot` folder, but you can develop it further to let the user upload a file before reading it, and to use other formats (such as RTF).
 
-A key point is that the Editor needs only the body of the content - full HTML cannot be edited, so you must make sure to remove the `html`, `head` elements, and to only provide the contents of the `body` tag without the tag itself. In this sample, the conversion is done as a `Fragment` export level, which keeps only the body and adds inline styles. If you use the full page export, by default styles go into a separate `<style>` tag and you should consider either including that, or using inline styles in the export settings.
+A key point is that the Editor needs only the body of the content - full HTML cannot be edited, so you must make sure to remove the `html`, `head` elements, and to only provide the contents of the `body` tag without the tag itself. In this sample, the conversion is done as a `Fragment` export level, which keeps only the body and adds inline styles - this is a feature of the Telerik DPL. If you use the full page export, by default styles go into a separate `<style>` tag and you should consider either including that (it might affect the rest of the page), or using inline styles in the export settings.
 
 The `ReadFile` helper function shows a sample way of tackling various formats and creating appropriate providers for them to import.
 
 Once the HTML string is prepared, we simply return it in the view-model and pass it to the editor `Value`.
 
-## PDF Export
+## Export
 
-The PDF export is done in the in the `FileConverter.cs` file - the `ExportHtmlFileToPdf` method.
+The export is done in the in the `FileConverter.cs` file - the `ExportAndDownloadHtmlContent` method.
 
-First, it creates the PDF file, and then with the use of a helper class (`FileDownloader.cs`) and a JavaScript function (in `fileDownloader.js`), it downloads that content in the browser.
+First, it creates the in-memory document with the HTML that we need for the conversion, then checks what provider to use based on the file exetension, and then - with the use of a helper class (`FileDownloader.cs`) and a JavaScript function (in `fileDownloader.js`) - it downloads that content in the browser.
 
-You can extend this further to export to more formats (make sure to use the proper MIME type if you download them), and/or save those files on the server.
+This also shows how to export to more formats (make sure to use the proper MIME type if you download them, also available in this sample), you can also extens it to save those files on the server.
 
-The export is triggered by a [custom tool](https://docs.telerik.com/blazor-ui/components/editor/custom-tool) that is placed at the beginning of the editor toolbar, you can invoke it in any way you prefer from the view.
+The export is triggered by a [custom tool](https://docs.telerik.com/blazor-ui/components/editor/custom-tool) that is placed at the beginning of the editor toolbar, you can invoke it in any way you prefer from the view (e.g., buttons outside of the editor or other events).
