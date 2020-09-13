63962637abdb68e40b89c620399e1542613e5b37
Marin Bratanov <m.bratanov@gmail.com>
2020-09-13 21:12:52 +0300

    feat(grid): remote validation example wasm wip

---
+++ grid/remote-validation/RemoteValidationWasm/RemoteValidationWasm/Shared/DropDownModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RemoteValidationWasm.Shared
+{
+    public class DropDownModel
+    {
+        public int? Id { get; set; }
+        public string Text { get; set; }
+    }
+}
