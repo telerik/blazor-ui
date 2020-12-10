2aa500518940a4a9b45403a97a5c93af7287dde8
Marin Bratanov <m.bratanov@gmail.com>
2020-12-10 14:29:58 +0200

    minor visual tweaks

--- scheduler/appointment-tooltips/Models/SchedulerAppointment.cs
+++ scheduler/appointment-tooltips/Models/SchedulerAppointment.cs
@@ -8,11 +8,21 @@ namespace appointment_tooltips.Models
     public class SchedulerAppointment
     {
         public int Id { get; set; }
+
         public string Title { get; set; }
+
         public string Description { get; set; }
+
         public DateTime Start { get; set; }
+
         public DateTime End { get; set; }
+
         public bool IsAllDay { get; set; }
+
         public string RecurrenceRule { get; set; }
+
+        public List<DateTime> RecurrenceExceptions { get; set; }
+
+        public Guid? RecurrenceId { get; set; }
     }
 }
