c6886da5d81a4eb83e152bcdfbf9b4bb927c26c0
Marin Bratanov <m.bratanov@gmail.com>
2020-12-17 15:14:05 +0200

    chore(schduler): commit the custom date-time validation attribute

---
+++ scheduler/custom-edit-form/Data/CompareDateAttribute.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace custom_edit_form.Data
+{
+    public class CompareDateAttribute : ValidationAttribute
+    {
+        public string compareToDateTimeProperty;
+
+        public CompareDateAttribute(string compareToPropertyName)
+        {
+            this.compareToDateTimeProperty = compareToPropertyName;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var validationObject = validationContext.ObjectInstance;
+            PropertyInfo propertyInfo = validationObject.GetType().GetProperty(compareToDateTimeProperty);
+
+            var currentValue = (DateTime?)value;
+            var compareToValue = (DateTime?)propertyInfo?.GetValue(validationObject);
+
+            return currentValue < compareToValue ? ValidationResult.Success : new ValidationResult(ErrorMessage);
+        }
+    }
+}
