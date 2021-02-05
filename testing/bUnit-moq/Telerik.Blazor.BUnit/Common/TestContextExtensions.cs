73c8ebc23f77685f8cb6a0a63a66a53254c7cfae
Joana <Joana.Ivanova@telerik.com>
2021-02-05 09:03:03 +0200

    chore(testing): add bunit and moq test project

---
+++ testing/bUnit-moq/Telerik.Blazor.BUnit/Common/TestContextExtensions.cs
@@ -0,0 +1,42 @@
+﻿using Bunit;
+using Moq;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.JSInterop;
+using Telerik.Blazor.Components;
+using Telerik.Blazor.Services;
+
+namespace Telerik.Blazor.BUnit.Common
+{
+    public static class TestContextExtensions
+    {
+        public static void AddTelerikBlazor(this TelerikTestContext context)
+        {
+            var jsRuntimeMock = new Mock<IJSRuntime>();
+            var localizerMock = new Mock<ITelerikStringLocalizer>();
+
+            context.Services.AddSingleton(jsRuntimeMock.Object);
+            context.Services.AddSingleton(localizerMock.Object);
+
+            context.Services.AddTelerikBlazor();
+
+            context.RootComponent = context.RenderComponent<TelerikRootComponent>();
+
+            context.RenderTree.TryAdd<CascadingValue<TelerikRootComponent>>(p =>
+            {
+                p.Add(parameters => parameters.IsFixed, true);
+                p.Add(parameters => parameters.Value, context.RootComponent.Instance);
+            });
+        }
+    }
+
+    /// <summary>
+    /// Creating a context to store the RootComponent. Alternatively in the tests you might have:
+    /// var myRootComponent = context.RenderComponent<TelerikRootComponent>(p => p.AddChildContent<Index>());
+    /// This will be required only when you'd like to access content injected in the RootComponent (ex. popups and window)
+    /// </summary>
+    public class TelerikTestContext : TestContext
+    {
+        public IRenderedComponent<TelerikRootComponent> RootComponent { get; set; }
+    }
+}
---
+++ testing/bUnit-moq/Telerik.Blazor.BUnit/DemoSample/WindowButtonPage.cs
@@ -0,0 +1,42 @@
+﻿using Bunit;
+using BUnit_Sample.Pages;
+using Microsoft.Extensions.DependencyInjection;
+using Telerik.Blazor.BUnit.Common;
+using Xunit;
+
+namespace Telerik.Blazor.BUnit
+{
+    public class WindowButtonPage
+    {
+        [Fact]
+        public void Button_in_window_is_rendered()
+        {
+            using var ctx = new TelerikTestContext();
+
+            ctx.AddTelerikBlazor();
+
+            ctx.RenderComponent<Window_Button>();
+
+            var button = ctx.RootComponent.Find("button[id=\"window-test-button\"]");
+            
+            Assert.Contains("Add Content", button.InnerHtml);
+        }
+
+        [Fact]
+        public void Button_in_window_click_action()
+        {
+            using var ctx = new TelerikTestContext();
+
+            ctx.AddTelerikBlazor();
+
+            ctx.RenderComponent<Window_Button>();
+
+            var button = ctx.RootComponent.Find("button[id=\"window-test-button\"]");
+
+            button.Click();
+
+            Assert.Contains("Custom Content", ctx.RootComponent.Markup);
+        }
+    }
+}
+
