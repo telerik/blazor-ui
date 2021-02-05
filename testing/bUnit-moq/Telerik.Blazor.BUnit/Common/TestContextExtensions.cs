bf77d008005d9812a62944bface7c09413225240
Marin Bratanov <m.bratanov@gmail.com>
2021-02-05 11:46:47 +0200

    docs: code comments

--- testing/bUnit-moq/Telerik.Blazor.BUnit/Common/TestContextExtensions.cs
+++ testing/bUnit-moq/Telerik.Blazor.BUnit/Common/TestContextExtensions.cs
@@ -12,16 +12,22 @@ namespace Telerik.Blazor.BUnit.Common
     {
         public static void AddTelerikBlazor(this TelerikTestContext context)
         {
-            var jsRuntimeMock = new Mock<IJSRuntime>();
-            var localizerMock = new Mock<ITelerikStringLocalizer>(); // you can also register a real one for actual localization to test that too
+            // mock the JS Interop service, you cannot use the one coming from the context
+            var jsRuntimeMock = new Mock<IJSRuntime>(); 
+
+            // you can also register a real one for actual localization to test that too
+            var localizerMock = new Mock<ITelerikStringLocalizer>(); 
 
             context.Services.AddSingleton(jsRuntimeMock.Object);
             context.Services.AddSingleton(localizerMock.Object);
 
-            context.Services.AddTelerikBlazor();
+            // add the Telerik Blazor services like in a regular app
+            context.Services.AddTelerikBlazor(); 
 
+            // add a Telerik Root Component to hold all Telerik components and other content
             context.RootComponent = context.RenderComponent<TelerikRootComponent>();
 
+            // provide the Telerik Root Component to the child components that need it (the Telerik components)
             context.RenderTree.TryAdd<CascadingValue<TelerikRootComponent>>(p =>
             {
                 p.Add(parameters => parameters.IsFixed, true);
