ba10986f110aff1bf8dee8af063bbbac85b3ba07
Eric D. Rohler <erohler@progress.com>
2021-02-13 07:34:27 -0500

    add kendo pdfjs pdf viewer widget example (#74)

--- common/kendo-in-blazor/KendoInBlazor/Startup.cs
+++ common/kendo-in-blazor/KendoInBlazor/Startup.cs
@@ -15,19 +15,27 @@ namespace KendoInBlazor
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
             Configuration = configuration;
+            Environment = env;
         }
 
         public IConfiguration Configuration { get; }
+        private readonly IWebHostEnvironment Environment;
 
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
-            services.AddServerSideBlazor();
+            services.AddServerSideBlazor(opts =>
+            {
+                if (Environment.IsDevelopment())
+                {
+                    opts.DetailedErrors = true;
+                }
+            });
             services.AddTelerikBlazor();
             services.AddSingleton<WeatherForecastService>();
         }
