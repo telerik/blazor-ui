ed70028f82111321387ec7e85f2f6adc58e1cf59
Marin Bratanov <m.bratanov@gmail.com>
2021-05-01 13:36:06 +0300

    feat(grid): example for custom validation on edited items from the state

---
+++ grid/validate-edit-item-from-grid-state/pre-validate-item-for-grid-state-init/Models/WeatherForecast.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace pre_validate_item_for_grid_state_init.Models
+{
+    public class WeatherForecast
+    {
+        public int Id { get; set; }
+
+        public DateTime Date { get; set; }
+
+        private double _tempC { get; set; }
+        public double TemperatureC
+        {
+            get
+            {
+                return _tempC;
+            }
+            set
+            {
+                _tempC = value;
+            }
+        }
+
+        public double TemperatureF
+        {
+            get
+            {
+                return 32 + (_tempC / 0.5556);
+            }
+            set
+            {
+                _tempC = (value - 32) * 0.5556;
+            }
+        }
+
+        [Required(ErrorMessage = "Your forecast requires a summary")]
+        [StringLength(255, ErrorMessage = "Summaries can be no more than 255 characters")]
+        public string Summary { get; set; }
+
+        public WeatherForecast()
+        {
+            Date = DateTime.Now.Date;
+        }
+    }
+}
