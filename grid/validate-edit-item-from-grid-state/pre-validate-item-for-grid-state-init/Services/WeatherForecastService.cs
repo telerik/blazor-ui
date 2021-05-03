e716e1838f7369afd01107dbf675708fdf82ab28
Marin Bratanov <marin-bratanov@users.noreply.github.com>
2021-05-03 10:46:29 +0300

    feat(grid): example for custom validation on edited items from the state (#87)

---
+++ grid/validate-edit-item-from-grid-state/pre-validate-item-for-grid-state-init/Services/WeatherForecastService.cs
@@ -0,0 +1,79 @@
+﻿using pre_validate_item_for_grid_state_init.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace pre_validate_item_for_grid_state_init.Services
+{
+    public class WeatherForecastService
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private List<WeatherForecast> _forecasts { get; set; }
+
+        public Task<List<WeatherForecast>> GetForecastListAsync(DateTime startDate)
+        {
+            if (_forecasts == null)
+            {
+                var rng = new Random();
+                _forecasts = Enumerable.Range(1, 150).Select(index => new WeatherForecast
+                {
+                    Id = index,
+                    Date = startDate.AddDays(index),
+                    TemperatureC = rng.Next(-20, 55),
+                    Summary = Summaries[rng.Next(Summaries.Length)]
+                }).ToList();
+            }
+
+            // detach the View data from the service for this example by creating a new pointer
+            List<WeatherForecast> cloneForTemplate = new List<WeatherForecast>(_forecasts);
+            return Task.FromResult<List<WeatherForecast>>(cloneForTemplate);
+        }
+
+        public async Task UpdateForecastAsync(WeatherForecast forecastToUpdate)
+        {
+            //implement proper error handling here, and then actual data source operations
+            if (_forecasts == null)
+            {
+                return;
+            }
+
+            var index = _forecasts.FindIndex(i => i.Id == forecastToUpdate.Id);
+            if (index != -1)
+            {
+                _forecasts[index] = forecastToUpdate;
+            }
+        }
+
+        public async Task DeleteForecastAsync(WeatherForecast forecastToRemove)
+        {
+            if (_forecasts == null) return;
+            //implement proper error handling here, and then actual data source operations
+
+            _forecasts.Remove(forecastToRemove);
+        }
+
+        public async Task InsertForecastAsync(WeatherForecast forecastToInsert)
+        {
+            //implement proper error handling here, and then actual data source operations
+            if (_forecasts == null)
+            {
+                return;
+            }
+
+            WeatherForecast insertedForecast = new WeatherForecast()
+            {
+                Id = _forecasts.Count + 1,
+                Date = forecastToInsert.Date,
+                TemperatureC = forecastToInsert.TemperatureC,
+                Summary = forecastToInsert.Summary
+            };
+
+            _forecasts.Insert(0, insertedForecast);
+        }
+    }
+}
