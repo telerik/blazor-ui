cd82e3a680c6288daca06d5c878500676eb2a662
Stamo-Gochev <stamo_gochev@yahoo.com>
2021-05-27 12:55:21 +0300

    feat: add maui example

---
+++ common/hybrid-blazor-apps/BlazorMauiApp/BlazorMauiApp/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+using System;
+
+namespace BlazorMauiApp
+{
+    public partial class MainPage : ContentPage, IPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
