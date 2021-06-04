a9a62756642b1da7a271fe1870481fb86d8f612c
Hristian Stefanov <72554148+xristianstefanov@users.noreply.github.com>
2021-06-04 11:37:31 +0300

    chore(grid):added custom validation in grid's editortemplate (#100)

---
+++ grid/custom-validation-in-editortemplate/RemoteValidationInGridEditorTemplate/Server/Controllers/MyValidationController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RemoteValidationInGridEditorTemplate.Shared;
+
+namespace RemoteValidationInGridEditorTemplate.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MyValidationController : ControllerBase
+    {
+        private readonly ILogger<MyValidationController> logger;
+
+        public MyValidationController(
+            ILogger<MyValidationController> logger)
+        {
+            this.logger = logger;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(SampleData myModel)
+        {
+            try
+            {
+                if (myModel.Name == "Nameee")
+                {
+                    ModelState.AddModelError(nameof(myModel.Name),
+                        "Name field cannot be 'Namee'.");
+                }
+                else if (string.IsNullOrEmpty(myModel.Name))
+                {
+                    ModelState.AddModelError(nameof(myModel.Name),
+                        "Name field is required.");
+                }
+                else
+                {
+                    logger.LogInformation("Processing the form asynchronously");
+
+                    // Process the valid form
+                    // async ...
+
+                    return Ok(ModelState);
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.LogError("Validation Error: {Message}", ex.Message);
+            }
+
+            return BadRequest(ModelState);
+        }
+    }
+}
---
+++ grid/custom-validation-in-editortemplate/RemoteValidationInGridEditorTemplate/Shared/SampleData.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RemoteValidationInGridEditorTemplate.Shared
+{
+    public class SampleData
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Role { get; set; }
+    }
+}
