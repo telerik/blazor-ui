d1ed6a5bbb2bbad5db42b1c0188d779a0ea66f9d
Marin Bratanov <m.bratanov@gmail.com>
2021-06-27 11:46:11 +0300

    feat(treeview): add rename node sample

---
+++ treeview/rename-node/rename-node/Models/TreeItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace rename_node.Models
+{
+    /// <summary>
+    /// Sample tree item model, uses the default field names.
+    /// </summary>
+    public class TreeItem
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public int? ParentId { get; set; }
+        public bool HasChildren { get; set; }
+        public string Icon { get; set; }
+        public bool Expanded { get; set; }
+    }
+}
