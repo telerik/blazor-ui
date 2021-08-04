cbcd0afc8bdeecb4af49a475ffd7f6ecb040751f
Hristian Stefanov <Hristian.Stefanov@progress.com>
2021-08-04 11:56:41 +0300

    chore(grid):added example project for saving state in wasm

---
+++ grid/save-state-in-wasm-through-controller/GridSavingStateInController/Server/Controllers/StateController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Telerik.Blazor.Components;
+using GridSavingStateInController.Shared;
+
+namespace GridSavingStateInController.Server.Controllers
+{
+    [ApiController]
+    public class StateController : ControllerBase
+    {
+        //We save the state for the demo in property. In real case, the state should be saved in a Database.
+        public static GridState<SampleData> GridCurrentState { get; set; }
+
+        [HttpPost]
+        [Route("setstate")]
+        public IActionResult SetState(GridState<SampleData> state)
+        {
+            var currentState = state;
+
+            GridCurrentState = currentState;
+
+            return Ok();
+        }
+        
+        [HttpGet]
+        [Route("getstate")]
+        public GridState<SampleData> GetState()
+        {
+            return GridCurrentState;
+        }
+    }
+}
---
+++ grid/save-state-in-wasm-through-controller/readme.md
@@ -0,0 +1,7 @@
+# Saving Grid State in a Blazor WebAssembly project through a controller
+
+This example shows how you can save the state of the Grid in a WASM project. Using a controller, we save the state in property for the demo. In a real case, the state should be saved in a **database**.
+
+The result from the example:
+
+![](images/grid-save-state-in-wasm.gif)
