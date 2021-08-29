d793d0ac566660d0c3efc17cca7a1fc32585c43d
Marin Bratanov <m.bratanov@gmail.com>
2021-08-29 03:33:58 +0300

    feat(grid): sample for scrolling to selected item

---
+++ grid/scroll-to-selected-row/scroll-to-selected-row/wwwroot/scroll-to-row.js
@@ -0,0 +1,13 @@
+﻿// this function takes a selector that describes the desired grid
+// and then finds the first selected row, then scrolls to it
+// via standard browser methods - effectively, the grid does not
+// interact or implement this. Suitable for regular paging mode only, not for virtual scrolling
+function scrollToSelectedRow(gridSelector) {
+    var gridWrapper = document.querySelector(gridSelector);
+    if (gridWrapper) {
+        var selectedRow = gridWrapper.querySelector("tr.k-state-selected");
+        if (selectedRow) {
+            selectedRow.scrollIntoView();
+        }
+    }
+}
