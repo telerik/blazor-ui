0c223aedfba471957dd94017ee77a2b995f7ada0
Stamo-Gochev <stamo_gochev@yahoo.com>
2021-09-16 12:50:11 +0300

    chore(hybrid-apps): add default blazor maui app for net 6 rc 1

---
+++ common/hybrid-blazor-apps/BlazorMauiApp/MauiProgram.cs
@@ -0,0 +1,30 @@
+﻿using BlazorMauiApp.Data;
+using Microsoft.AspNetCore.Components.WebView.Maui;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Maui;
+using Microsoft.Maui.Controls.Compatibility;
+using Microsoft.Maui.Controls.Hosting;
+using Microsoft.Maui.Hosting;
+
+namespace BlazorMauiApp
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .RegisterBlazorMauiWebView()
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                });
+
+            builder.Services.AddBlazorWebView();
+            builder.Services.AddSingleton<WeatherForecastService>();
+
+            return builder.Build();
+        }
+    }
+}
