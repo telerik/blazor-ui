e75cfd1c045bd0f29c2ff7c9a0157762c8a23211
Radko Stanev <49035696+radkostanev@users.noreply.github.com>
2022-06-03 11:12:21 +0300

    chore(testing): add a test that overrides input's debouncedelay (#175)

---
+++ testing/bUnit-justmock/Telerik.Blazor.BUnit.JustMock/DemoSample/TextBoxPage.cs
@@ -0,0 +1,30 @@
+﻿using Bunit;
+using BUnit_Sample.Pages;
+using Telerik.Blazor.BUnit.JustMock.Common;
+using Telerik.Blazor.Components;
+using Xunit;
+
+namespace Telerik.Blazor.BUnit.JustMock
+{
+    public class TextBoxPage : TelerikTestContext
+    {
+        [Fact]
+        public void OnInput_updates_value()
+        {
+            var expected = "this is the new textbox value";
+            var textBoxPage = RenderComponent<TextBox>();
+
+            var textBox = textBoxPage.FindComponent<TelerikTextBox>();
+
+            // Override the default DebounceDelay so oninput event is executed immediately, 
+            // as we do not want to have a deliberate delay in our unit tests.
+            textBox.SetParametersAndRender(parameters => parameters.Add(p => p.DebounceDelay, 0));
+
+            var textBoxElement = textBoxPage.Find("#tested-textbox");
+
+            textBoxElement.Input(expected);
+
+            Assert.Equal(expected, textBox.Instance.Value);
+        }
+    }
+}
