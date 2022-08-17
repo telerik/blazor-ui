9590402a7a4034826e82403209f5c9dabacbfa3e
Svetoslav Dimitrov <svdimitr@progress.com>
2022-08-17 10:30:33 +0300

    chore(window-as-a-service): add a demo application that showcases how to use the TelerikWindow as a service.

---
+++ window/WindowAsAService/Models/WindowInstance.cs
@@ -0,0 +1,23 @@
+﻿using Telerik.Blazor.Components;
+
+namespace WindowAsAService.Models
+{
+    public class WindowInstance
+    {
+        public bool Visible { get; set; } = true;
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public string Width { get; set; }
+
+        public string Height { get; set; }
+
+        /// <summary>
+        /// This is the reference of the TelerikWindow component instance.
+        /// It can be used after the first render follwoing the creation.
+        /// </summary>
+        public TelerikWindow WindowRef { get; set; }
+    }
+}
