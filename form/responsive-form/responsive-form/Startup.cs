549a946ab2761b95232704e310a02069782c2f16
radkostanev <radkostanev@gmail.com>
2023-09-25 17:07:20 +0300

    chore(responsive-form): bump sdk and product version

--- form/responsive-form/responsive-form/Startup.cs
+++ form/responsive-form/responsive-form/Startup.cs
@@ -1,15 +1,9 @@
 ﻿using BlazorPro.BlazorSize;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace responsive_form
 {
