a7a3d7c5b8608a36608814ea160e07f5f58a6c83
Dimo Dimov <961014+dimodi@users.noreply.github.com>
2023-11-21 13:00:10 +0200

    update OData Wasm App

--- grid/odata/WasmApp/Client/Program.cs
+++ grid/odata/WasmApp/Client/Program.cs
@@ -1,23 +1,13 @@
-﻿
+﻿using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.DependencyInjection;
-using System.Threading.Tasks;
-using System.Net.Http;
-using System;
+using WasmApp.Client;
 
-namespace WasmApp.Client
-{
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
-            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
-            builder.Services.AddTelerikBlazor();
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-            await builder.Build().RunAsync();
-        }
-    }
-}
+builder.Services.AddTelerikBlazor();
+
+await builder.Build().RunAsync();
