f1a20f9f6b11fb237944e986368123e67ef3314c
Dimo Dimov <961014+dimodi@users.noreply.github.com>
2024-09-30 16:56:33 +0300

    Update WASM localization example

---
+++ common/localization/ClientLocalizationResx/Services/SampleResxLocalizer.cs
@@ -0,0 +1,23 @@
+﻿using ClientLocalizationResx.Resources;
+using Telerik.Blazor.Services;
+
+namespace ClientLocalizationResx.Services
+{
+    public class SampleResxLocalizer : ITelerikStringLocalizer
+    {
+        // This indexer is required
+        public string this[string name]
+        {
+            get
+            {
+                return GetStringFromResource(name);
+            }
+        }
+
+        // Sample implementation that uses .resx files in ~/Resources named TelerikMessages.<culture-locale>.resx
+        public string GetStringFromResource(string key)
+        {
+            return TelerikMessages.ResourceManager.GetString(key, TelerikMessages.Culture) ?? string.Empty;
+        }
+    }
+}
