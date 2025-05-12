5595becebe0b72d9108df7793ce81cfa337f0916
Radko Stanev <49035696+radkostanev@users.noreply.github.com>
2025-05-12 17:48:41 +0300

    Update sample-applications/blazor-financial-dashboard/BlazorFinancialDashboard/Data/PaymentMethod.cs
    Co-authored-by: Yanislav Ivanov <93312843+yanisslav@users.noreply.github.com>

--- sample-applications/blazor-financial-dashboard/BlazorFinancialDashboard/Data/PaymentMethod.cs
+++ sample-applications/blazor-financial-dashboard/BlazorFinancialDashboard/Data/PaymentMethod.cs
@@ -2,8 +2,7 @@
 {
 	public class PaymentMethod
 	{
-		public int Id { get; set; }
-
-		public string Name { get; set; } = string.Empty;
+	public int Id { get; set; }
+	public string Name { get; set; } = string.Empty;
 	}
 }
