fd4a24cd3dd19e4917379805d53eed68b132b0f7
Hristian Stefanov <Hristian.Stefanov@progress.com>
2026-04-02 16:26:44 +0300

    chore(SampleApps): add healthcare app

---
+++ sample-applications/blazor-healthcare-app/BlazorHealthcareApp/Program.cs
@@ -0,0 +1,29 @@
+using BlazorHealthcareApp.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddTelerikBlazor();
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+app.UseHttpsRedirection();
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
